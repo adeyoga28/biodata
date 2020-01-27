ffb40cac6978c8c62241ea99ec5d846e807d9cd9
ade yoga <57425861+adeyoga28@users.noreply.github.com>
2020-01-27 12:16:10 +0700

    Add files via upload

---
+++ BIODATA/src/biodata/BIODATA.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package biodata;
+
+/**
+ *
+ * @author windbit10
+ */
+public class BIODATA {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       String nama = "Ade Yoga Pranata";
+        String Semester = "1";
+        String Nim = "201969040076";
+        String kelas = "C";
+        String ttl = "Pasurua,28 Mei 1998";
+        String alamat = "Pandansari Sumberrejo Pasuruan";
+        String Agama = "Islam";
+        String Hobi = "Membaca";
+        String cita = "Swasta";
+        String Motto = "Bermanfaat bagi orang banyak";
+        
+        System.out.println("Nama\t\t:"+nama);
+        System.out.println("Semester\t:"+Semester);
+        System.out.println("Nim:\t\t:"+Nim);
+        System.out.println("Kelas\t\t:"+kelas);
+        System.out.println("Tempat,Tgl Lahir:"+ttl);
+        System.out.println("Alamat\t\t:"+alamat);
+        System.out.println("Agama\t\t:"+Agama);
+        System.out.println("Hobi\t\t:"+Hobi);
+        System.out.println("Pekerjaan\t:"+cita);
+        System.out.println("Purpose Of My Life:"+Motto);
+    
+}}
